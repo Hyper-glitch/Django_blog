524299007f22e29951d65a98bf56cefbcdb86ad0
Roman <graff049@gmail.com>
2021-09-05 15:05:54 +0300

    feat: add posts and routs

--- social_web/blog/views.py
+++ social_web/blog/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'mr.Smirnov',
+        'title': 'Blog post 1',
+        'content': 'First post content',
+        'data_posted': 'August 29, 2021'
+    },
+    {
+        'author': 'mr.Twister',
+        'title': 'Blog post 2',
+        'content': 'Second post content',
+        'data_posted': 'August 30, 2021'
+    }
+]
+
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html')
