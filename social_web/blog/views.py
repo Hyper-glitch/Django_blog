22ea6cc57a83f8c71b1fb576e2dc65bb5545613d
Roman <graff049@gmail.com>
2021-09-05 20:14:06 +0300

    feat: add and register Post class for posts in blog

--- social_web/blog/views.py
+++ social_web/blog/views.py
@@ -1,25 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-posts = [
-    {
-        'author': 'mr.Smirnov',
-        'title': 'Blog post 1',
-        'content': 'First post content',
-        'data_posted': 'August 29, 2021'
-    },
-    {
-        'author': 'mr.Twister',
-        'title': 'Blog post 2',
-        'content': 'Second post content',
-        'data_posted': 'August 30, 2021'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
