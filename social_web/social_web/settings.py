33d7d770407224f971bbbea344c867e4853746fd
Roman <graff049@gmail.com>
2021-10-13 21:24:13 +0300

    feat: 1. add password_reset_complete template 2. add DEFAULT_FROM_EMAIL 3. add new url password-reset-complete

--- social_web/social_web/settings.py
+++ social_web/social_web/settings.py
@@ -143,3 +143,4 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
+DEFAULT_FROM_EMAIL = 'SocialWeb Team <noreply@socialweb.com>'
